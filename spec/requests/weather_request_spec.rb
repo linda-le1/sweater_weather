447fc0c5579d3bd78b882596062583a72d438d6c
Linda Le <yueling1222@gmail.com>
2020-03-01 18:51:42 -0700

    Add test for coordinates and route

--- spec/requests/weather_request_spec.rb
+++ spec/requests/weather_request_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 describe 'Weather' do
-    it 'can returns' do
+    it 'can return weather forecast based on location' do
 
+        get '/api/v1/forecast?location=denver,co'
+
+        expect(response).to be_successful
+
+        coordinates = JSON.parse(response.body)['results']
+
+        expect(expect(coordinates[0]['location']['lat']).to eql('39.7392358')
+        expect(expect(coordinates[0]['location']['lng']).to eql('-104.990251')
     end
 end
