9d46a152785762e90a6eb5e3878668df3c63cb09
Linda Le <yueling1222@gmail.com>
2020-03-02 11:52:11 -0700

    Add additional refactor to weather service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -7,8 +7,7 @@ class WeatherService
         response = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@location}&key=#{ENV['GEOCODE_API']}")
         parsed_location = JSON.parse(response.body)['results']
         location = Location.new(parsed_location)
-        weather_response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARKSKY_API']}/#{location.latitude},#{location.longitude}?exclude=minutely")
-        JSON.parse(weather_response.body)
+        retrieve_data("#{ENV['DARKSKY_API']}/#{location.latitude},#{location.longitude}?exclude=minutely")
     end
 
     def get_future_weather(latitude, longitude, time)
