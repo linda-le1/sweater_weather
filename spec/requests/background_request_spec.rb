d646abf74a280b937ec68ef0b4ffbba213f7d649
Linda Le <yueling1222@gmail.com>
2020-03-02 09:18:53 -0700

    Add test for munchies endpoint

--- spec/requests/background_request_spec.rb
+++ spec/requests/background_request_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Background Image" do
-  it 'can return background image based on location' do
+RSpec.describe "Munchie Endpoint" do
+  it 'will return food and forecast information for a destination city' do
     WebMock.allow_net_connect!
 
-    location = 'denver,co'
-    # get "/api/v1/backgrounds?location=#{location}"
+    start = 'denver,co'
+    destination = 'pueblo,co'
+    cuisine = 'chinese'
 
-    # expect(response).to be_successful
-    # background = JSON.parse(response.body)
+    visit "/api/v1/munchies?start=#{start}&end=#{destination}&food=#{cuisine}"
+
+    expect(response).to be_successful
+
+    destination_info = JSON.parse(response.body)['data']['attributes']
+
+    expect(destination_info).to have_key('end_location')
+    expect(destination_info).to have_key('travel_time')
+    expect(destination_info).to have_key('forecast')
+    expect(destination_info).to have_key('restaurant')
+
+    expect(destination_info['restaurant'].count).to eq(1)
+    expect(destination_info['restaurant']).to have_key('name')
+    expect(destination_info['restaurant']).to have_key('address')
   end
 end
