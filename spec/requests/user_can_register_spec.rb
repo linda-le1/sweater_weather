05a5e769fb686c02fe2a39fe0543fc920a3af00d
Linda Le <yueling1222@gmail.com>
2020-03-02 23:40:31 -0700

    Add sad path if passwords do not match

--- spec/requests/user_can_register_spec.rb
+++ spec/requests/user_can_register_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'Users' do
         expect(user_info['data']['attributes']['api_key']).not_to eql(nil)
     end
 
-    it 'can return a 400 response if user cannot be created' do
+    it 'can return a 400 response if email already exists' do
       User.create(email: 'whatever@example.com', password: '11111', password_confirmation: '11111')
       params = {
                   email: "whatever@example.com",
@@ -31,8 +31,25 @@ RSpec.describe 'Users' do
     expect(response.status).to eq(400)
 
     user_info = JSON.parse(response.body)
-                byebug
-    expect(user_info['data']['attributes']).to have_key("error")
-    expect(user_info['data']['attributes']['error']).to eql('Email has already been taken')
+
+    expect(user_info).to have_key("error")
+    expect(user_info['error']).to eql('Email has already been taken')
+  end
+
+  it 'can return a 400 response if user did not enter password' do
+    params = {
+                  email: "whatever@example.com",
+                  password: "password",
+                  password_confirmation: "111"
+                }
+
+    post '/api/v1/users', params: params
+
+    expect(response.status).to eq(400)
+
+    user_info = JSON.parse(response.body)
+
+    expect(user_info).to have_key("error")
+    expect(user_info['error']).to eql("Password confirmation doesn't match Password")
   end
 end
