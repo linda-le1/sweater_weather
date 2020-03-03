eb8ed96c2c8aaf15d99256b3f2bc8ea1d1ef198f
Linda Le <yueling1222@gmail.com>
2020-03-02 23:24:54 -0700

    Add secure token method for api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     validates_presence_of :password_digest, require: true
     validates :email, uniqueness: true, presence: true
     has_secure_password
-
+    has_secure_token :api_key
 end
