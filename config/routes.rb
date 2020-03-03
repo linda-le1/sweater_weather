c3eb27ef0058ba7cb597ac6748f6325e7300ee97
Linda Le <yueling1222@gmail.com>
2020-03-02 23:03:13 -0700

    Add post route and controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
         get '/forecast', to: 'weather#index'
         get '/backgrounds', to: 'backgrounds#index'
+        post '/api/v1/users', to : 'users#create'
       end
     end
 end
