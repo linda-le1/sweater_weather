cd63df1d6765478708ba1f46347900c1674c61b9
Linda Le <yueling1222@gmail.com>
2020-03-02 23:23:52 -0700

    Add index to api key column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_061308) do
+ActiveRecord::Schema.define(version: 2020_03_03_062215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_061308) do
     t.string "api_key"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
   end
 
 end
