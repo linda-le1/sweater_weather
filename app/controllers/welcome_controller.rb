2901a9ec6ba60659c33bb283a50d1ad303c3fb06
Linda Le <yueling1222@gmail.com>
2020-03-03 18:34:55 -0700

    Fix index method on welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,7 @@
 class WelcomeController < ApplicationController
 
     def index
-        render json: {
-            'Welcome to Sweather Weather. For more information on endpoints,
-            visit https://github.com/linda-le1/sweater_weather'
-                    }, status: :ok
+        render json: 'Welcome to Sweather Weather. For documentation, visit https://github.com/linda-le1/sweater_weather.'
     end
 
 end
