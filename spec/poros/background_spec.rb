99ba2d0875b9dc45e316c479593343ea20596795
Linda Le <yueling1222@gmail.com>
2020-03-03 21:58:03 -0700

    Add background poro test

---
+++ spec/poros/background_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Background do
+    it 'exists and has certain attributes' do
+        location = 'Denver,CO'
+        data = UnsplashService.new(location).get_json
+        background_poro = Background.new(data)
+
+        expect(background_poro).to be_a Background
+        expect(background_poro.id).to be(nil)
+        expect(background_poro.url).to eq('https://unsplash.com/photos/N_tcaOWTSQQ')
+    end
+end
